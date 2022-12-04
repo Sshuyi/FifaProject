d715c35ce802b6bb121b0821326ba24e009385a0
Sshuyi <shuyi1140@gmail.com>
2022-12-04 13:23:38 -0500

    2nd Main class

--- src/com/qa/fifa/Main.java
+++ src/com/qa/fifa/Main.java
@@ -3,8 +3,7 @@ package com.qa.fifa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("I added a new in this project");
 	}
 
 }
